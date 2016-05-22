fb319568789eb4d95828deae0349a7c7e22a198f
the-new-sky <lorisazerty@gmail.com>
2016-05-22 17:21:50 +0200

    Initial commit
    Add PlugIn.py

---
+++ PlugIn.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+__author__ = "@the_new_sky"
+__license__ = "MIT"
+
+class LoadPlugins:
+
+    def __init__(self, plugin_dir):
+
+        sys.path.append(os.path.abspath(plugin_dir))
+
+        plugin_list = os.listdir(plugin_dir)
+        self.plugin_list = []
+
+        for plugin in plugin_list:
+            if not plugin.endswith('__'):
+                self.plugin_list.append(os.path.splitext(plugin)[0])
+
+    def import_all(self):
+        self.uselist = list(map(__import__, self.plugin_list))
+
+    @property
+    def plugins(self):
+        return self.uselist
